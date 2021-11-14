f22bdf69f9323c29df84c911d2c3a0f659d38bc1
Christian M <chriamue@gmail.com>
2021-11-14 21:56:52 +0100

    :sparkles: added dashboard plugin

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ COPY package*.json ./
 RUN npm install
 RUN npm install --global lisk-commander
 COPY . .
-EXPOSE 4000
+EXPOSE 4000 4004 4005
+#./bin/run start --api-ws --enable-dashboard-plugin --enable-http-api-plugin --http-api-plugin-port 4000 --http-api-plugin-whitelist 0.0.0.0/0 --api-ws --api-ws-port 8080
+#CMD [ "./bin/run", "start", "--api-ws", "--enable-dashboard-plugin", "--enable-http-api-plugin", "--http-api-plugin-port", "4000", "--http-api-plugin-whitelist", "0.0.0.0/0", "--api-ws", "--api-ws-port", "8080"]
 CMD [ "./bin/run", "start"]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Application, PartialApplicationConfig } from 'lisk-sdk';
+import { Application, PartialApplicationConfig, utils } from 'lisk-sdk';
 import { registerModules } from './modules';
 import { registerPlugins } from './plugins';
 
@@ -6,7 +6,32 @@ export const getApplication = (
 	genesisBlock: Record<string, unknown>,
 	config: PartialApplicationConfig,
 ): Application => {
-	const app = Application.defaultApplication(genesisBlock, config);
+
+	const appConfig = utils.objects.mergeDeep({}, config, {
+		rpc: { enable: true, mode: "ws", host: "0.0.0.0" },
+		plugins: {
+			httpApi: {
+				host: "0.0.0.0",
+				port: 4000,
+				whiteList: [],
+				public: true,
+				cors: {
+					origin: '*',
+					methods: ['GET', 'POST', 'PUT'],
+				},
+			},
+			dashboard: {
+				applicationUrl: 'ws://localhost:8080/ws',
+				port: 4005,
+				host: '0.0.0.0',
+				applicationName: 'Lisk',
+			}
+		}
+	});
+
+	console.log(appConfig);
+
+	const app = Application.defaultApplication(genesisBlock, appConfig);
 
 	registerModules(app);
 	registerPlugins(app);
